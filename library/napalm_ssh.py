3e70d35c0c1a6e65397d5fe3f1f9c6128aa62aa6
Kenkun <kenneth.acar.garcia@gmail.com>
2021-08-08 11:30:26 +0800

    Updated output reporting

--- library/napalm_ssh.py
+++ library/napalm_ssh.py
@@ -20,10 +20,10 @@ def napalm_ssh(driver,node,cli_cmd,username,password,mode):
         for i in output:
             wr_file.write( i + '\n' + output[i] + '\n')
         wr_file.close()
-        return print(f'  {node[1]} {mode} collection success!')
+        return print(f'  {node[1]} {mode} via napalm ssh data collection success!')
 
     except (ConnectionException) as error:
         wr_file = open( 'logs/' + node[1] + '_' + node[2] + '_' + timestamp + '.' + mode, 'w' )
         wr_file.write(str(error))
         wr_file.close()
-        return print(f'{node[1]} {mode} collection failed!')
+        return print(f'{node[1]} {mode} via napalm ssh data collection failed!')
