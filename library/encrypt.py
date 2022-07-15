e6b362601a7609008c71664c6192b380df4c77dd
kagarcia1618 <kenneth.acar.garcia@gmail.com>
2022-07-15 11:51:10 +0800

    Updated encrypt script

--- library/encrypt.py
+++ library/encrypt.py
@@ -5,7 +5,7 @@ def generate_key():
     Generates a key and save it into a file
     """
     key = Fernet.generate_key()
-    with open(".secret.key", "wb") as key_file:
+    with open("private/.secret.key", "wb") as key_file:
         key_file.write(key)
 
 def load_key():
